baba96f0e268d8bfe1705712831c2bf4881c6746
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-09-26 19:51:40 +0545

    pattern

--- Fundamentals/pattern/squarepattern.cpp
+++ Fundamentals/pattern/squarepattern.cpp
@@ -23,11 +23,19 @@ int main(){
         }
         cout<<endl;
     }
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=n; j++){
-            cout<<j;
-        }
-        cout<<endl;
+   
+   for(int i = 1; i<=n; i++){
+    for(int j=1; j<=n; j++){
+        cout<<j;
     }
+    cout<<endl;
+   }
+   for(int i = 1; i<=n; i++){
+    for(int j = 1; j<=i; j++){
+        cout<<j;
+    }
+    cout<<endl;
+   }
+   
     return 0;
 }
