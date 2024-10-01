a77ef55c091d122bd86625cf0bd1bfa1fdfd4f1b
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-01 16:14:33 +0545

    squarepattern

--- Fundamentals/pattern/squarepattern.cpp
+++ Fundamentals/pattern/squarepattern.cpp
@@ -1,41 +1,50 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n = 4;
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=n; j++){
+      int n = 4;
+
+      for(int i = 0; i<n; i++){
+        for(int j=0; j<n; j++){
             cout<<"*";
         }
         cout<<endl;
-    }
-
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=n; j++){
-            cout<<j;
+      }
+    
+      for(int i = 0; i<n; i++){
+          int a = 1;
+        for(int j = 0; j<n; j++){
+            cout<<a;
+            a++;
         }
         cout<<endl;
-    }
-    for(int i = 1; i<= n; i++){
+      }
+
+      for(int j =0; j<n; j++){
         char ch = 'A';
-        for(int j = 1; j<=n; j++){
+        for(int i = 0; i<n; i++){
             cout<<ch;
             ch++;
         }
         cout<<endl;
-    }
-   
-   for(int i = 1; i<=n; i++){
-    for(int j=1; j<=n; j++){
-        cout<<j;
-    }
-    cout<<endl;
-   }
-   for(int i = 1; i<=n; i++){
-    for(int j = 1; j<=i; j++){
-        cout<<j;
-    }
-    cout<<endl;
-   }
-   
+      }
+
+      int a = 1;
+      for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<a<<" ";
+            a++;
+        }
+        cout<<endl;
+      }
+      
+      char ch = 'A';
+       for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+       }
+
     return 0;
 }
