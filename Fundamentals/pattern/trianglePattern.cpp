106fdc16560ab51329b70f424d81ef4ea201cf25
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-01 20:47:03 +0545

    triangle pattern

---
+++ Fundamentals/pattern/trianglePattern.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i=1; i<n; i++){
+        for(int j =0; j<i; j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+
+    for(int i= 1; i<n; i++){
+        for(int j = 1; j<i+1; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j= 0; j<i+1; j++){
+            cout<<char(65+i);
+        }
+        cout<<endl;
+    }
+
+    for(int i = 1; i<n; i++){
+        for(int j = 1; j<i+1; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
