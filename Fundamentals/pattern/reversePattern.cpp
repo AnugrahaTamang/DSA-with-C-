b1081f5fdf066a3830b6328c267246622a3a152c
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-01 21:05:20 +0545

    reversepattern

---
+++ Fundamentals/pattern/reversePattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i=n; i>0; i--){
+        cout<<i<<endl;
+    }
+
+    for(int i = 0; i<n; i++){
+        for(int j = i; j>0; j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+    for(int i= 0; i<n; i++){
+        for(int j = i+1; j>0; j--){
+            cout<<char(64+j);
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
